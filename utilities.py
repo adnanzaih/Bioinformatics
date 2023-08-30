b5f9175dd462a9135bc5538b9e50550cf35c3284
adnanzaih <Adnan>
2023-08-29 21:24:26 -0400

    updates

---
+++ utilities.py
@@ -0,0 +1,34 @@
+def colored(seq):
+    bcolors = {
+        'A': '\033[92m',
+        'C': '\033[94m',
+        'G': '\033[93m',
+        'T': '\033[91m',
+        'U': '\033[91m',
+        'reset': '\033[0;0m'
+    }
+
+    tmpStr = ""
+
+    for nuc in seq:
+        if nuc in bcolors:
+            tmpStr += bcolors[nuc] + nuc
+        else:
+            tmpStr += bcolors['reset'] + nuc
+
+    return tmpStr + '\033[0;0m'
+
+def read_FASTA(filepath):
+    with open(filepath, 'r') as f:
+        FASTAFile = [l.strip() for l in f.readlines()]
+
+    FASTADict = {}
+    FASTALabel = ""
+
+    for line in FASTAFile:
+        if '>' in line:
+            FASTALabel = line
+            FASTADict[FASTALabel] = ""
+        else:
+            FASTADict[FASTALabel] += line
+    return FASTADict
